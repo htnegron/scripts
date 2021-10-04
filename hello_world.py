b9321057623876c0ab1b2e7e56c150415dad3b26
htnegron <90418114+htnegron@users.noreply.github.com>
2021-10-04 15:51:50 -0500

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+print('hello world')
+print("hello world")
+print("hello world")
+# print quotes
+print("she said 'hello world'")
+print('she said \'hello world\'')
+#print variables
+first_name = 'bob'
+last_name = 'dylan'
+print(first_name)
+print(first_name, last_name)
+print(first_name,last_name)
+print(first_name.upper())
+sum = 2+4
+print(sum)
+print('The answer is',sum)
